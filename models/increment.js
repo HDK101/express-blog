00b444de2912bec5bafe7a98a9aa9f34f89e01f4
Lucas Hideki <lucashideki87@gmail.com>
2020-01-29 17:24:07 -0300

    Set default values for increment

--- models/increment.js
+++ models/increment.js
@@ -1,11 +1,11 @@
 const { Schema, model } = require("mongoose");
 
 const increment = new Schema({
-  post: Number,
-  admin: Number,
-  user: Number,
-  comment: Number,
-  id: String
+  post: { type: Number, default: 0},
+  admin: { type: Number, default: 0},
+  user: { type: Number, default: 0},
+  comment: { type: Number, default: 0},
+  id: { type: String, default: 0},
 });
 
 const Increment = model("Increment", increment);
