28ea308a64b40df0324f68313ab9da3a70cc4f02
Lucas Hideki <lucashideki87@gmail.com>
2020-01-29 17:25:19 -0300

    Increment model fixed

--- models/increment.js
+++ models/increment.js
@@ -5,7 +5,7 @@ const increment = new Schema({
   admin: { type: Number, default: 0},
   user: { type: Number, default: 0},
   comment: { type: Number, default: 0},
-  id: { type: String, default: 0},
+  id: { type: String, default: "increment"},
 });
 
 const Increment = model("Increment", increment);
