de78989e79cc4e7c024728926c9f3a04e9962b6c
Lucas Hideki <lucashideki87@gmail.com>
2020-01-31 17:14:48 -0300

    Simple test

---
+++ test/controllers.test.js
@@ -0,0 +1,13 @@
+const { adminLoginByToken } = require("../controller/controllers");
+const assert = require("assert");
+
+beforeEach(function() {
+    return console.log("Hey medic, i am big chungus");
+});
+
+describe("Tests", function() {
+    it('should test', () => {
+        assert.equal(1, 1);
+    });
+});
+
