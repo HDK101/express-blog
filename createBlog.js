8072c23ebb1f854a191c5e4d9db87f79fada2036
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2020-01-31 21:35:37 -0200

    createBlog now uses readline module instead of args

--- createBlog.js
+++ createBlog.js
@@ -1,18 +1,84 @@
 const { Admin, Increment } = require("./models/models");
 const { setConfig } = require("./Server/configParser");
+const readline = require("readline");
 const mongoose = require("mongoose");
 
-if (!process.argv[2] || !process.argv[3] || !process.argv[4]) throw "Empty name, email or password!";
-
-mainAdminDb = new Admin({
-  email: process.argv[2],
-  name: process.argv[3],
-  password: process.argv[4],
-  main: true,
-  id: 0
-});
+/*Models */
+var mainAdminDb;
 incrementDb = new Increment();
 
+/*Main admin credentials*/
+var adminName, adminEmail, adminPassword;
+
+/*Blog settings*/
+var blogName, blogPort;
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+/*****************
+Questions promises
+*****************/
+function askAdminName() {
+  promise = new Promise(function(resolve) {
+    reader.question("What's the main admin name?\n", function(answer) {
+      console.log("Okay then.");
+      adminName = answer;
+      resolve();
+    });
+  });
+  return promise;
+}
+
+function askAdminEmail() {
+  promise = new Promise(function(resolve) {
+    reader.question("What's the main admin email?\n", function(answer) {
+      console.log("Okay then.");
+      adminEmail = answer;
+      resolve();
+    });
+  });
+  return promise;
+}
+
+function askAdminPassword() {
+  promise = new Promise(function(resolve) {
+    reader.question("What's the main admin password?\n", function(answer) {
+      console.log("Okay then.");
+      adminPassword = answer;
+      resolve();
+    });
+  });
+  return promise;
+}
+
+function askBlogName() {
+  promise = new Promise(function(resolve) {
+    reader.question("Blog name?(or codename?)\n", function(answer) {
+      console.log("Blog name set to " + answer);
+      blogName = answer;
+      resolve();
+    });
+  });
+  return promise;
+}
+
+function askBlogPort() {
+  promise = new Promise(function(resolve) {
+    reader.question("Which port should be used?\n", function(answer) {
+      console.log("Port set to " + answer);
+      blogPort = answer;
+      resolve();
+    });
+  });
+  return promise;
+}
+
+/********************
+Configuration promise
+********************/
 function setConfigJSON() {
   promise = new Promise(function(resolve, reject) {
     function generateKey() {
@@ -23,12 +89,22 @@ function setConfigJSON() {
       return key;
     }
     const generatedKey = generateKey();
-    config = { port: 3000, secretKey: generatedKey, initialized: true };
+    config = {
+      name: blogName,
+      port: blogPort,
+      secretKey: generatedKey,
+      initialized: true
+    };
     setConfig(config);
     resolve();
   });
   return promise;
 }
+
+/****************
+Database promises
+****************/
+
 
 function initializeMongoose() {
   promise = new Promise(function(resolve, reject) {
@@ -60,6 +136,13 @@ function checkAdminExistence() {
 
 function createAdmin(count) {
   promise = new Promise(function(resolve, reject) {
+    mainAdminDb = new Admin({
+      name: adminName,
+      email: adminEmail,
+      password: adminPassword,
+      main: true,
+      id: 0
+    });
     if (count == 0) {
       console.log("No main admin was found, creating...");
       mainAdminDb.save(function(err) {
@@ -102,12 +185,19 @@ function createIncrement(count) {
   return promise;
 }
 
-initializeMongoose()
+/*Question promises*/
+askAdminName()
+  .then(askAdminEmail)
+  .then(askAdminPassword)
+  .then(askBlogName)
+  .then(askBlogPort)
   .then(setConfigJSON)
+  .then(initializeMongoose)
   .then(checkAdminExistence)
   .then(createAdmin)
   .then(checkIncrementExistence)
   .then(createIncrement)
+  .then(function() {})
   .finally(function() {
     console.log("Everything done!");
     process.exit();
