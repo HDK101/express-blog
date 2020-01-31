c8e36cb59f58f2ffea7b069b10b68db28841204f
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2020-01-31 21:37:54 -0200

    configParser now overwrites instead

--- Server/configParser.js
+++ Server/configParser.js
@@ -16,15 +16,10 @@ let config = {};
  * (NOTE 2: secretKey should be generated randomly)
  */
 function setConfig(configToSet) {
-  config = JSON.parse(fs.readFileSync(__dirname + "/config.json"));
-  if (config.initialized == "false") {
-    config = Object.assign(config, configToSet);
-    configJSON = JSON.stringify(config);
-    fs.writeFileSync(__dirname + "/config.json",configJSON);
-    console.log("Configuration file created!");
-  } else {
-    console.log("Configuration file already created!");
-  }
+  config = Object.assign(config, configToSet);
+  configJSON = JSON.stringify(config);
+  fs.writeFileSync(__dirname + "/config.json", configJSON);
+  console.log("Configuration file created!");
 }
 
 /**
