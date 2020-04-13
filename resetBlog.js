00dbcc337ac5d65f2c18c0786648d4c28cda093b
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2020-04-13 14:07:10 -0300

    resetBlog remade

--- resetBlog.js
+++ resetBlog.js
@@ -1,5 +1,4 @@
-const { Admin, Increment } = require("./models/models");
-const { getConfig, resetConfig } = require("./Server/configParser");
+const { resetConfig, getConfig } = require("./Server/configParser");
 const readline = require("readline");
 const mongoose = require("mongoose");
 
@@ -7,26 +6,42 @@ const { blogName } = getConfig();
 
 const reader = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-function askReset(value) {
-  promise = new Promise(function(resolve, reject) {
-    reader.question(`Are you sure you want to clear "${blogName}"?\n`, function(
+/*****************
+Questions promises
+*****************/
+function askDeleteDb() {
+  promise = new Promise(function (resolve, reject) {
+    reader.question(`Are you sure you want to clear ${blogName}\n`, function (
       answer
     ) {
       if (answer == "yes" || answer == "y" || answer == "Y") resolve();
-      else if (answer == "no" || answer == "n" || answer == "N") {
-        reject("Reset cancelled.");
-      }
-      reader.close();
+      if (answer == "" || answer == "no" || answer == "n" || answer == "N")
+        reject("Cancel");
     });
   });
   return promise;
 }
 
+/*********************
+Configuration promises
+*********************/
+
+function clearConfig() {
+  promise = new Promise(function (resolve) {
+    resetConfig();
+    resolve();
+  });
+}
+
+/****************
+Database promises
+****************/
+
 function initializeMongoose() {
-  promise = new Promise(function(resolve, reject) {
+  promise = new Promise(function (resolve, reject) {
     mongoose.set("useNewUrlParser", true);
     mongoose.set("useFindAndModify", false);
     mongoose.set("useCreateIndex", true);
@@ -34,45 +49,26 @@ function initializeMongoose() {
 
     mongoose.connect(`mongodb://localhost/${blogName}_blog`);
     const db = mongoose.connection;
-    db.on("error", function() {
+    db.on("error", function () {
       reject("Connection failure!");
     });
-    db.on("open", function() {
+    db.on("open", function () {
+      db.dropDatabase();
       resolve();
     });
   });
   return promise;
 }
 
-function clearConfiguration() {
-  promise = new Promise(function(resolve) {
-    resetConfig();
-    resolve();
-  });
-  return promise;
-}
-
-function clearDB() {
-  promise = new Promise(function(resolve) {
-    const db = mongoose.connection;
-    db.dropDatabase(function(err) {
-      if (err) reject(err);
-      console.log("Database cleared!");
-      resolve();
-    });
-  });
-  return promise;
-}
-
-askReset()
+/*Question promises*/
+askDeleteDb()
+  .then(clearConfig)
   .then(initializeMongoose)
-  .then(clearConfiguration)
-  .then(clearDB)
-  .finally(function() {
+  .then(function () {
     console.log("Everything done!");
     process.exit();
   })
-  .catch(function(error) {
-    if (error != "Reset cancelled.") console.log("Error: " + error);
-    else console.log(error);
+  .catch(function (error) {
+    console.log("Error: " + error);
+    process.exit();
   });
